1926c813ea2822578a47b0959ab3d856ea055727
Shashwat Singh <shashwat123student@gmail.com>
2024-06-28 01:04:37 +0530

    Serialized and Decerialized data

---
+++ protobuf/src/index.ts
@@ -0,0 +1,17 @@
+const protobuf = require('protobufjs');
+
+
+protobuf.load('./message.proto')
+    // @ts-ignore
+    .then(root => {
+        const Person = root.lookupType('Person');
+        const person = { name: "Shashwat Singh", age: 19 };
+        const buffer = Person.encode(person).finish();
+        require('fs').writeFileSync('person.bin', buffer);
+        console.log('Person serialized and saved to person.bin');
+        const data = require('fs').readFileSync('person.bin');
+        const deserializedPerson = Person.decode(data);
+
+        console.log('Person deserialized from person.bin:', deserializedPerson);
+    })
+    .catch(console.error);
